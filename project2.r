1fcd10e7ec290a8b1c67c64bd1d5d3ba3a495b30
vaisang <vaisang@dukes.jmu.edu>
2019-02-09 12:49:16 -0500

    Update project2.r
    created scatter plot detailing nameplate capacity and operating year of plants

--- project2.r
+++ project2.r
@@ -16,12 +16,41 @@ head(wnd_data)
 tail(wnd_data)
 sapply(wnd_data,class)
 
-                     
-levels(wnd_data$COUNTY)
-nlevels(wnd_data$COUNTY)
-                     
-                     
+#number of states operating in                     
+levels(wnd_data$STATE)
+nlevels(wnd_data$STATE)
+
+#number of Facilities
+nlevels(wnd_data$FACILITY_CODE)
+
+#Range of nameplate capacity
+range(wnd_data$NAMEPLATE)
+#Range of operating year
+range(wnd_data$OPERATING_YEAR)                     
                 
-                     
-                     
+#plot of nameplate cap vs operating year 
+plot(wnd_data$OPERATING_YEAR, wnd_data$NAMEPLATE)
+
+# r
+r <- cor(wnd_data$OPERATING_YEAR, wnd_data$NAMEPLATE)
+# sy
+sy <- sd(wnd_data$NAMEPLATE)
+# sx
+sx <- sd(wnd_data$OPERATING_YEAR)
+#  ybar
+ybar <- mean(wnd_data$NAMEPLATE)
+#  xbar
+xbar <- mean(wnd_data$OPERATING_YEAR)
+#  slope
+slope <- (ybar / xbar)
+int <- (ybar - slope * sx)
+int
+
+
+
+
+
+
+
+
                                                          
