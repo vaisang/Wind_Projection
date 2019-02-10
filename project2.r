f189b1253b8c09498bb3409d0c6854f0171830ad
vaisang <vaisang@dukes.jmu.edu>
2019-02-09 21:39:56 -0500

    Update project2.r
    added separate files for 15-17 data

--- project2.r
+++ project2.r
@@ -1,4 +1,4 @@
-#wind projection#
+#wind projection-----------------------------------------------
 getwd()
 
 setwd("C:/Program Files/Git/dev/R")
@@ -8,19 +8,49 @@ wd
 dir()
 list.files()
 
-# Read CSV into R
+# Read full data CSV into R
 wnd_data <- read.csv(paste(wd, "Windprojection.csv", sep="/"))
-
 class(wnd_data)
 head(wnd_data)
 tail(wnd_data)
 sapply(wnd_data,class)
+
+#2015 data
+wnd15 <- read.csv(paste(wd, "wndproj2015.csv", sep = "/"))
+class(wnd15)
+head(wnd15)
+tail(wnd15)
+sapply(wnd15,class)
+
+#2016 data
+wnd16 <- read.csv(paste(wd, "wndproj2016.csv", sep = "/"))
+class(wnd16)
+head(wnd16)
+tail(wnd16)
+sapply(wnd16,class)
+
+#2017 data 
+wnd17 <- read.csv(paste(wd, "wndproj2017.csv", sep = "/"))
+class(wnd17)
+head(wnd17)
+tail(wnd17)
+sapply(wnd17,class)
+
+
+#plotting all data
+library(gridExtra)
+grid.table(wnd_data)
+
 
 #number of states operating in                     
 levels(wnd_data$STATE)
 nlevels(wnd_data$STATE)
 
 #number of Facilities
+wnd_data$FACILITY_CODE <- as.factor(wnd_data$FACILITY_CODE)
+sapply(wnd_data, class)
+
+levels(wnd_data$FACILITY_CODE)
 nlevels(wnd_data$FACILITY_CODE)
 
 #Range of nameplate capacity
@@ -46,11 +76,4 @@ slope <- (ybar / xbar)
 int <- (ybar - slope * sx)
 int
 
-
-
-
-
-
-
-
-                                                         
+#plotting facilities on map
